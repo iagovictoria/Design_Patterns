50a002d42bba19136116cf053c84b3d1aeea92e1
iagovictoria <iago_victoria@hotmail.co.jp>
2018-11-06 20:29:23 -0200

    Alteração no Exec_1_1

--- Adapter/src/Exec_1_1/HashMatriz.java
+++ Adapter/src/Exec_1_1/HashMatriz.java
@@ -4,10 +4,8 @@ import java.util.HashMap;
 public class HashMatriz<T,S> extends HashMap<T,S>{
 
 
-	public HashMatriz(Object[][] matriz) {
-		for(int c = 0; c < matriz.length; c++) {
-		
-		}
+	public HashMatriz(Object matriz[][]) {
+		//this.put(matriz[0][0], matriz[1][1]);
 	}
 	
 
